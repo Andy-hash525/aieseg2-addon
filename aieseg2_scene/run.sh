a6ae2ba9388e5cde90db9b03a867471e977261fd
Andy-hash525 <wealthyhawk@gmail.com>
2026-06-21 19:09:55 +0900

    Update run.sh

--- aieseg2_scene/run.sh
+++ aieseg2_scene/run.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/with-contenv bashio
-
 echo "RUN.SH STARTED"
-
 MQTT_USER=$(bashio config 'mqtt_user')
 MQTT_PASSWORD=$(bashio config 'mqtt_password')
-
 echo "MQTT_USER=$MQTT_USER"
-
 export MQTT_USER
 export MQTT_PASSWORD
-
 python3 /app/app.py
